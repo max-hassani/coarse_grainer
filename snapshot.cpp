9781e2f9993c74803aad7626f47225d7c52173a3
muh-hassani <mhassani86@gmail.com>
2021-09-06 14:42:34 +0200

    updating snapshot class

--- snapshot.cpp
+++ snapshot.cpp
@@ -13,7 +13,6 @@ snapshot::snapshot(int n){
 }
 snapshot::snapshot(int n , box BX){
 	flag_init = true;
-	flag_box_init = true;
 	n_atoms = n;
 	atoms = (atom*) malloc(n_atoms * sizeof(atom));
 	bx = BX;
@@ -21,13 +20,13 @@ snapshot::snapshot(int n , box BX){
 snapshot::~snapshot(){
 	free(atoms);
 }
-void snapshot::load_dump(char *fname, bool flag_wrap, bool flag_norm, bool flag_charge, bool flag_vel)
+void snapshot::load_dump(bool flag_wrap, bool flag_norm, bool flag_charge, bool flag_vel)
 {
 	int iniLine = 5;
-	ifstream inputFile(fname);
+	ifstream inputFile(dump_file_name);
 	if (!inputFile)
 	{
-		cout << "ERROR: no input file: " << fname << endl;
+		cout << "ERROR: no input file: " << dump_file_name << endl;
 		exit(1);
 	}
 	string temp;
@@ -35,13 +34,18 @@ void snapshot::load_dump(char *fname, bool flag_wrap, bool flag_norm, bool flag_
 		getline(inputFile,temp);
 	}
 	if(!flag_box_init){
+		cout << "here here" << endl;
 		inputFile >> bx.x_min >> bx.x_max ;
 		bx.cal_LX();
+		cout << "LX: " << bx.lx << endl;
 		inputFile >> bx.y_min >> bx.y_max ;
 		bx.cal_LY();
+		cout << "LY: " << bx.ly << endl;
 		inputFile >> bx.z_min >> bx.z_max ;
 		bx.cal_LZ();
+		cout << "LZ: " << bx.lz << endl;
 	}else{
+		cout << "unfortunately here" << endl;
 		for(int i = 0; i < 3; i++){
 			getline(inputFile,temp);
 		}
@@ -50,20 +54,29 @@ void snapshot::load_dump(char *fname, bool flag_wrap, bool flag_norm, bool flag_
 	cout << temp<< endl;
 	getline(inputFile,temp);
 	int tempInd;
-	double x, y, z;
+	double x, y, z, tmpVal;
+
+
+	cout << "bx.x: " << bx.lx << " " << bx.x_min << " " << bx.x_max << endl;
+	cout << "bx.y: " << bx.ly << " " << bx.y_min << " " << bx.y_max << endl;
+	cout << "bx.z: " << bx.lz << " " << bx.z_min << " " << bx.z_max << endl;
+
 	for(int i = 0; i < n_atoms; i++){
 		inputFile >> tempInd;
 		if(!flag_vel){
 			if(flag_charge){
-				inputFile >> atoms[tempInd - 1].type >>  x >> y >> z >> atoms[tempInd - 1].charge;
+				inputFile >> atoms[tempInd - 1].type >>  x >> y >> z >> tmpVal >> tmpVal >> tmpVal >>  atoms[tempInd - 1].charge;
 			}else{
-				inputFile >> atoms[tempInd - 1].type >>  x >> y >> z;
+				inputFile >> atoms[tempInd - 1].type >>  x >> y >> z >> tmpVal >> tmpVal >> tmpVal;
+				if(tempInd < 10){
+					cout << "ind: " << tempInd - 1 << "\t" << atoms[tempInd - 1].type << "\t" <<  x << "\t" << y << "\t" << z << endl;
+				}
 			}
 		}else{
 			if(flag_charge){
-				inputFile >> atoms[tempInd - 1].type >>  x >> y >> z >> atoms[tempInd - 1].vel.x >> atoms[tempInd - 1].vel.y >> atoms[tempInd - 1].vel.z >> atoms[tempInd - 1].charge;
+				inputFile >> atoms[tempInd - 1].type >>  x >> y >> z >> atoms[tempInd - 1].vel.x >> atoms[tempInd - 1].vel.y >> atoms[tempInd - 1].vel.z >> tmpVal >> tmpVal >> tmpVal >> atoms[tempInd - 1].charge;
 			}else{
-				inputFile >> atoms[tempInd - 1].type >>  x >> y >> z >> atoms[tempInd - 1].vel.x >> atoms[tempInd - 1].vel.y >> atoms[tempInd - 1].vel.z;
+				inputFile >> atoms[tempInd - 1].type >>  x >> y >> z >> atoms[tempInd - 1].vel.x >> atoms[tempInd - 1].vel.y >> atoms[tempInd - 1].vel.z >> tmpVal >> tmpVal >> tmpVal;
 			}
 		}
 		if(!flag_wrap){
@@ -73,7 +86,7 @@ void snapshot::load_dump(char *fname, bool flag_wrap, bool flag_norm, bool flag_
 				atoms[tempInd - 1].posU.z = z;
 				atoms[tempInd - 1].pos = atoms[tempInd - 1].apply_pbc(bx);
 			}else{
-				cout << "error: " << endl;
+				cout << "error: when the positions are normalized, wrapped positions are expected!!!" << endl;
 				exit(2);
 			}
 		}else{
@@ -82,11 +95,38 @@ void snapshot::load_dump(char *fname, bool flag_wrap, bool flag_norm, bool flag_
 				atoms[tempInd - 1].pos.y = y;
 				atoms[tempInd - 1].pos.z = z;
 			}else{
-				atoms[tempInd - 1].pos.x = x;
-				atoms[tempInd - 1].pos.y = y;
-				atoms[tempInd - 1].pos.z = z;
-				atoms[tempInd - 1].pos = atoms[tempInd - 1].revert_norm(bx);
+					atoms[tempInd - 1].pos.x = x;
+					atoms[tempInd - 1].pos.y = y;
+					atoms[tempInd - 1].pos.z = z;
+					atoms[tempInd - 1].revert_norm(bx);
 			}
 		}
 	}
+}
+void snapshot::set_dumpFile_path(string dump_path, int t){
+  size_t spac;
+  string sub1;
+  string sub2;
+  spac = dump_path.find("*");
+  sub1 = dump_path.substr(0,spac);
+  sub2 = dump_path.substr(spac+1);
+  //cout << dump_path << endl;
+  //cout << sub1 << endl;
+  //cout << sub2 << endl;
+  char arr2[100];
+  char arr3[100];
+  /*for(int i = 0; i < sub1.length(); i++){
+    arr2[i] = sub1[i];
+  }
+  for(int i = 0; i < sub2.length(); i++){
+    arr3[i] = sub2[i];
+  }*/
+  //cout << arr2 << endl;
+  strcpy(arr2, sub1.c_str());
+  strcpy(arr3, sub2.c_str());
+  sprintf(dump_file_name, "%s%d%s",arr2,t,arr3);
+  cout << dump_file_name << endl;
+  int n = sub1.length()+sub2.length()+7;
+  //cout << "size: " << n << endl;
+  dump_file_name[n]='\0';
 }
--- snapshot.hh
+++ snapshot.hh
@@ -8,10 +8,11 @@ public:
 	bool flag_box_init= false;
 	box bx;
 	atom *atoms;
-
+	char dump_file_name[1000];
 	snapshot(int n);
 	snapshot(int n, box BX);
 	~snapshot();
-	void load_dump(char *file_name, bool flag_wrap, bool flag_norm, bool flag_charge, bool flag_vel);
+	void load_dump(bool flag_wrap, bool flag_norm, bool flag_charge, bool flag_vel);
 	void load_data(char *file_name);
+	void set_dumpFile_path(string dump_path, int t);
 };
