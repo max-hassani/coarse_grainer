8cac3513c428ea89079cd01306025be84555c18d
muh-hassani <mhassani86@gmail.com>
2021-09-06 13:27:30 +0200

    adding main.cpp

---
+++ main.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <fstream>
+#include "snapshot.hh"
+#include "p_vec_ten.hh"
+#include <cstdlib>
+#include <algorithm>
+#include <time.h>
+#include <string.h>
+#include "input.hh"
+
+using namespace std;
+typedef p_vec<> Vec;
+
+int main(int argc, char **argv){
+	if(argc !=2){
+			cout << "the path to the input file is missing from the given argument!!!" << endl;
+		  return 1;
+
+	}
+	char input_path[1000];
+	strcpy(input_path, argv[1]);
+
+	cout << "the input path: " << input_path << endl;
+	input inp(input_path);
+	inp.read();
+	box bx;
+	snapshot config(inp.NRPART,bx);
+
+	return 0;
+}
